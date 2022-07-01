f1bc36d0dbf15f6d717dc84189b0cf7abec92fe5
glorious73 <amjadmh73@gmail.com>
2022-07-01 22:18:16 +0300

    Successfully split sidebar and content inside main.

---
+++ src/js/Views/ContentView.js
@@ -0,0 +1,25 @@
+const template = document.createElement('template');
+
+template.innerHTML = /*html*/`
+    <div>
+        <h1>Content is here!</h1>
+    </div>
+`;
+
+export default class ContentView extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        console.log("Content view attached.");
+    }
+
+    disconnectedCallback() {
+
+    }
+}
+
+window.customElements.define('content-view', ContentView);
--- src/js/Views/MainView.js
+++ src/js/Views/MainView.js
@@ -1,12 +1,13 @@
 import SidebarView from "./SidebarView";
+import ContentView from "./ContentView";
 
 const template = document.createElement('template');
 
 template.innerHTML = /*html*/`
     <link rel="stylesheet" href="${localStorage.getItem("cssFileName")}">
-    <sidebar-view class="sidebar"></sidebar-view>
-    <div style="width:50vw; border:red solid 2px; padding:30px;">
-        <h1>Hello World!</h1>
+    <div class="body-container">
+        <sidebar-view class="sidebar"></sidebar-view>
+        <content-view class="content"></content-view>
     </div>
 `;
 
