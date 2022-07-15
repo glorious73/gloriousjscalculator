bdab87ac1dfed1ae13d88220f52d99613427bc65
glorious73 <amjadmh73@gmail.com>
2022-07-15 23:03:25 +0300

    (integer happy scenario + clear) working

--- src/js/Component/Calculator/CalculatorApp.js
+++ src/js/Component/Calculator/CalculatorApp.js
@@ -7,29 +7,29 @@ template.innerHTML = /*html*/`
     <div class="calculator-result" data-bind="result">0</div>
     <div class="calculator-pad">
         <!-- First row -->
-        <button class="operator-button" data-action="clear">AC</button>
-        <button class="operator-button" data-action="square">x<sup>2</sup></button>
-        <button class="operator-button" data-action="squareroot">√x</button>
-        <button class="operator-button" data-action="divide">÷</button>
+        <button class="operator-button" data-operation="clear">AC</button>
+        <button class="operator-button" data-operation="square">x<sup>2</sup></button>
+        <button class="operator-button" data-operation="squareroot">√x</button>
+        <button class="operator-button" data-operation="divide">÷</button>
         <!-- Second row -->
         <button class="operand-button">7</button>
         <button class="operand-button">8</button>
         <button class="operand-button">9</button>
-        <button class="operator-button" data-action="multiply">X</button>
+        <button class="operator-button" data-operation="multiply">X</button>
         <!-- Third row -->
         <button class="operand-button">4</button>
         <button class="operand-button">5</button>
         <button class="operand-button">6</button>
-        <button class="operator-button" data-action="subtract">−</button>
+        <button class="operator-button" data-operation="subtract">−</button>
         <!-- Fourth row -->
         <button class="operand-button">1</button>
         <button class="operand-button">2</button>
         <button class="operand-button">3</button>
-        <button class="operator-button" data-action="add">+</button>
+        <button class="operator-button" data-operation="add">+</button>
         <!-- Fifth row -->
         <button class="operand-zero-button">0</button>
-        <button class="operand-button">•</button>
-        <button class="operator-button" data-action="calculate">=</button>
+        <button class="operand-button" data-operation="decimal">•</button>
+        <button class="operator-button" data-operation="calculate">=</button>
     </div>
 `;
 
@@ -39,7 +39,11 @@ export default class CalculatorApp extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         // application variable
-        this.result = new Observable(""); // string since operators will also be shown
+        this.result          = new Observable(""); // string since operators will also be shown
+        this.firstValue      = 0;
+        this.secondValue     = 0;
+        this.operator        = undefined;
+        this.previousKeyType = '';
     }
 
     connectedCallback() {
@@ -47,8 +51,8 @@ export default class CalculatorApp extends HTMLElement {
         // Observables
         this.shadowRoot.querySelectorAll("[data-bind]").forEach(observable => this.createTwoWayBinding(observable));
         // Calculator events
-        this.shadowRoot.querySelectorAll("button").forEach(button => {
-            button.addEventListener("click", () => this.updateResult(button.innerText));
+        this.shadowRoot.querySelectorAll("button").forEach(calculatorKey => {
+            calculatorKey.addEventListener("click", () => this.updateResult(calculatorKey));
         });
     }
 
@@ -63,8 +67,70 @@ export default class CalculatorApp extends HTMLElement {
         });
     }
 
-    updateResult(value) {
-        this.result.value += value;
+    updateResult(calculatorKey) {
+        const operation = calculatorKey.dataset.operation;
+        if(!operation) {
+            this.previousKeyType = 'number';
+            this.result.value += calculatorKey.innerText;
+        }
+        else {
+            if(operation === 'decimal')
+                this.result.value = this.result.value;
+                //this.result.value += '.';
+            else
+                if(operation === 'calculate')
+                    this.calculate();
+                else if(operation === 'clear')
+                    this.clear();
+                else
+                    this.setOperation(calculatorKey, operation);
+        }
+    }
+
+    setOperation(calculatorKey, operation) {
+        if(!this.operator) {
+            this.previousKeyType = 'operator';
+            this.operator = operation;
+            this.result.value += ` ${calculatorKey.innerText} `;
+        }
+    }
+
+    canCalculate() {
+        return (this.previousKeyType === 'number' && this.operator);
+    }
+
+    calculate() {
+        if(this.canCalculate()) {
+            // 1. get values
+            const calculation = this.result.value;
+            const calculationInputs = calculation.split(" ");
+            this.firstValue = calculationInputs[0];
+            this.secondValue = calculationInputs[2];
+            // 2. do operation
+            let result = '';
+            const firstVal = parseFloat(this.firstValue);
+            const secondVal = parseFloat(this.secondValue);
+            if (this.operator === 'add') {
+                result = firstVal + secondVal;
+            } else if (this.operator === 'subtract') {
+                result = firstVal - secondVal;
+            } else if (this.operator === 'multiply') {
+                result = firstVal * secondVal;
+            } else if (this.operator === 'divide') {
+                result = firstVal / secondVal;
+            }
+            // 3. display result
+            this.result.value += ` = ${result}`;
+        }
+    }
+
+    clear() {
+        this.result.value    = '';
+        this.firstValue      = 0;
+        this.secondValue     = 0;
+        this.operator        = undefined;
+        this.previousKeyType = '';
+        this.shadowRoot.querySelector('[data-bind="result"]').innerHTML = '0';
     }
 }
 
