d0669a03ab74a65e5c028a154927e5a293243324
glorious73 <amjadmh73@gmail.com>
2022-07-21 22:56:00 +0300

    updateView (still not done)

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,14 @@ import Router from './Router/Router';
 // Views
 import MainView from './Views/MainView';
 import SidebarView from './Views/SidebarView';
+// Pages
+import CalculatorPage from './Views/Pages/CalculatorPage';
+import ContentView from './Views/ContentView';
+import ScientificCalculatorPage from './Views/Pages/ScientificCalculatorPage';
+import CurrencyExchangePage from './Views/Pages/CurrencyExchangePage';
+import TemperaturePage from './Views/Pages/TemperaturePage';
+import DatePage from './Views/Pages/DatePage';
+import TimePage from './Views/Pages/TimePage';
 
 
 const app = new App("#app");
@@ -16,9 +24,47 @@ app.addComponent({
 });
 
 app.addComponent({
+    name: 'content-view',
+    view: ContentView
+});
+
+app.addComponent({
     name: 'sidebar-view',
     view: SidebarView
 });
 
+// Add Pages
+app.addComponent({
+    name: 'calculator-page',
+    view: CalculatorPage
+});
+app.addComponent({
+    name: 'scientificcalculator-page',
+    view: ScientificCalculatorPage
+});
+app.addComponent({
+    name: 'currency-page',
+    view: CurrencyExchangePage
+});
+app.addComponent({
+    name: 'temperature-page',
+    view: TemperaturePage
+});
+app.addComponent({
+    name: 'date-page',
+    view: DatePage
+});
+app.addComponent({
+    name: 'time-page',
+    view: TimePage
+});
+
 // Add routes
 router.addRoute('main-view', "^$");
+router.addRoute('main-view', "^#/$");
+router.addRoute('calculator-page', "^#/calculator$");
+router.addRoute('scientificcalculator-page', "^#/scientificcalculator$");
+router.addRoute('currency-page', "^#/currencyexchange$");
+router.addRoute('temperature-page', "^#/temperaturecalculator$");
+router.addRoute('date-page', "^#/datecalculator$");
+router.addRoute('time-page', "^#/timecalculator$");
